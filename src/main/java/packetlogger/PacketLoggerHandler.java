6e6b90658682f7b5ae2a39de02892463370926af
k <146610232+xxdiddybludxx67@users.noreply.github.com>
2025-08-31 00:37:35 +1000

    Update PacketLoggerHandler.java

--- src/main/java/packetlogger/PacketLoggerHandler.java
+++ src/main/java/packetlogger/PacketLoggerHandler.java
@@ -7,7 +7,6 @@ import net.minecraft.network.Packet;
 
 public class PacketLoggerHandler extends ChannelDuplexHandler {
 
-    // Note: PacketLoggerMod.interceptor is expected to be non-null and set up in init()
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         try {
@@ -17,11 +16,9 @@ public class PacketLoggerHandler extends ChannelDuplexHandler {
                 PacketLoggerMod.interceptor.logIncomingPacket(p);
             }
         } catch (Throwable t) {
-            // don't break pipeline on logging errors
             t.printStackTrace();
         }
 
-        // continue chain
         super.channelRead(ctx, msg);
     }
 
@@ -29,16 +26,14 @@ public class PacketLoggerHandler extends ChannelDuplexHandler {
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
         try {
             if (msg instanceof Packet && PacketLoggerMod.interceptor != null) {
-                // outbound from client -> server
                 Packet<?> p = (Packet<?>) msg;
                 PacketLoggerMod.interceptor.logOutgoingPacket(p);
             }
         } catch (Throwable t) {
-            // don't break pipeline on logging errors
             t.printStackTrace();
         }
 
-        // continue chain
         super.write(ctx, msg, promise);
     }
 }
+
